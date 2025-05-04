d77ad10d5615d34b73d65aaa2777fa7a7ebc581b
HarshMPatel11 <146648004+HarshMPatel11@users.noreply.github.com>
2025-05-05 00:13:07 +0530

    Delete Memories-App-MERN-master directory

--- Memories-App-MERN-master/client/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-// import libraries from redux
-import { combineReducers } from 'redux';
-// import posts and auth file
-import posts from './posts';
-import auth from './auth';
-
-export const reducers = combineReducers({ posts, auth });
